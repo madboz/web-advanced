b337342fdd6545566db532064c526e7d0414aaa7
bozim435 <bozim435@newschool.edu>
2016-10-06 23:38:41 +0000

    fixed snowflake for sure

--- demos/generate/js/main.js
+++ demos/generate/js/main.js
@@ -1,16 +1,17 @@
 $("body").css("background-color","#C6E2FF");
 
-for (var e = 0; e < 1000; e++){
-    
-    $("body").append("<div class='box'></div>");
-    
+for (var i = 0; i < 16; i++) {
+    $("body").append("<div class='snowflake'></div>");
 }
 
-for (var i = 0; i < 10; i++){
-    
-    $("body").append("<div class='snowflake'></div>");
-    
-}
+$(".snowflake").each(function(){
+    var crystalcount = Math.floor(Math.random()*14) + 6;
+    for(var c = 0; c < crystalcount; c++) {
+        $(this).append("<div class='crystal'></div>");
+    }
+    $(this).css("top",Math.floor(Math.random()*1000)+"px");
+    $(this).css("left",Math.floor(Math.random()*1000)+"px");
+});
 
 $(window).resize(function() {
     $(".snowflake").each(function(){
@@ -19,31 +20,30 @@ $(window).resize(function() {
     });
 });
 
-var opacity = 0;
 
-$(".box").each(function(){
-    
-  opacity += .001
-  $(this).css("opacity", opacity)
-  console.log("found box" + opacity);
-    
-});
-
-
-$(".snowflake").each(function(){
-    
+$(".snowflake").each(function() {
     var rotation = 0;
     var crystallength = $(this).find(".crystal").length;
-    $(this).find(".crystal").each(function(){
-        
-        var newrotation = "rotate(" + rotation +"deg)";
+    $(this).find(".crystal").each(function() {
+        var newrotation = "rotate(" + rotation + "deg)";
         $(this).css("transform", newrotation);
-        rotation += 180/crystallength; 
-        
+        rotation += 180/crystallength;
     })
-    
-    
 });
+
+
+for (var i = 0; i < 1000; i++) {
+    $("body").append("<div class='box'></div>");
+}
+
+var opacity = 0;
+
+$(".box").each(function() {
+    opacity += .001;
+    $(this).css("opacity",opacity);
+    console.log("found box " + opacity);
+});
+
 
 // $("window").resize(function(){
     
